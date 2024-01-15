cafb5af6b2cadc9baab2a55720a4d7ddac00ee28
Saufi <msaufi2325@gmail.com>
2024-01-16 02:28:25 +0900

    Add CORS support and configure routes

--- cmd/api/main.go
+++ cmd/api/main.go
@@ -1,5 +1,29 @@
 package main
 
-func main() {
+import (
+	"fmt"
+	"log"
+	"net/http"
+)
 
+const webPortNumber = "80"
+
+type Config struct{}
+
+func main() {
+	app := Config{}
+
+	log.Printf("Starting broker service on port %s\n", webPortNumber)
+
+	// define http server
+	srv := &http.Server{
+		Addr:    fmt.Sprintf(":%s", webPortNumber),
+		Handler: app.routes(),
+	}
+
+	// start http server
+	err := srv.ListenAndServe()
+	if err != nil {
+		log.Panic(err)
+	}
 }
